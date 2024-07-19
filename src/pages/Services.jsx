5b65cc43712faa9d2bfb9675c0e1a3f5e0a2a9de
Ashley_hue <ashleysmarshall90@gmail.com>
2024-07-19 23:49:55 +0300

    made front pages responsive

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from 'react';
 import './Services.css';
-import sale from "../components/Assets/sell.png"
+import sale from "../components/Assets/sell.png";
 import techsupport from "../components/Assets/sup.png";
 import repair from "../components/Assets/repair.png";
 import fabricate from "../components/Assets/fabr.png";
 import training from "../components/Assets/train.png";
 import maintenance from "../components/Assets/maintain.png";
-import servicesbg from "../components/Assets/servicesbgg.jpeg"
-import phonecall from "../components/Assets/phonecall.png"
-import processimage from "../components/Assets/prroc.png"
+import servicesbg from "../components/Assets/servicesbgg.jpeg";
+import phonecall from "../components/Assets/phonecall.png";
+import processimage from "../components/Assets/prroc.png";
 import procimage from "../components/Assets/procp.png";
 import proceimage from "../components/Assets/procd.png";
 
@@ -16,7 +16,7 @@ const Services = () => {
   return (
     <div className="services-container">
       <div className="services-bg">
-        <img src={servicesbg} alt="" />
+        <img src={servicesbg} alt="Services Background" />
         <div className="service-overlay">
           <div className="service-content">
             <h3>Services</h3>
@@ -27,36 +27,36 @@ const Services = () => {
       <div className="services">
         <div className="top-cards">
           <div className="detail-card">
-            <img src={sale} alt="" className="service-logo" />
+            <img src={sale} alt="Equipment Sales" className="service-logo" />
             <h4>Equipment Sales</h4>
           </div>
           <div className="detail-card">
-            <img src={techsupport} alt="" className="service-logo" />
+            <img src={techsupport} alt="Technical Support" className="service-logo" />
             <h4>Technical Support</h4>
           </div>
           <div className="detail-card">
-            <img src={repair} alt="" className="service-logo" />
+            <img src={repair} alt="Machine Repair" className="service-logo" />
             <h4>Machine Repair</h4>
           </div>
         </div>
         <div className="top-cards">
           <div className="detail-card">
-            <img src={fabricate} alt="" className="service-logo" />
+            <img src={fabricate} alt="Custom Fabrication" className="service-logo" />
             <h4>Custom Fabrication</h4>
           </div>
           <div className="detail-card">
-            <img src={training} alt="" className="service-logo" />
+            <img src={training} alt="Training and Consultation" className="service-logo" />
             <h4>Training and Consultation</h4>
           </div>
           <div className="detail-card">
-            <img src={maintenance} alt="" className="service-logo" />
+            <img src={maintenance} alt="Maintenance Plans" className="service-logo" />
             <h4>Maintenance Plans</h4>
           </div>
         </div>
       </div>
       <div className="callcentre">
         <div className="appointment">
-          <img src={phonecall} alt="" className="phoneicon" />
+          <img src={phonecall} alt="Phone Icon" className="phoneicon" />
           <div className="text">
             <p>GET A FREE CONSULTATION</p>
             <h4>0724621977</h4>
@@ -78,13 +78,13 @@ const Services = () => {
         </h3>
         <div className="process-icon-container">
           <div>
-            <img src={processimage} alt="" className="process-icon" />
+            <img src={processimage} alt="Process Step 1" className="process-icon" />
           </div>
           <div>
-            <img src={procimage} alt="" className="process-icon" />
+            <img src={procimage} alt="Process Step 2" className="process-icon" />
           </div>
           <div>
-            <img src={proceimage} alt="" className="process-icon" />
+            <img src={proceimage} alt="Process Step 3" className="process-icon" />
           </div>
         </div>
       </div>
@@ -92,4 +92,4 @@ const Services = () => {
   );
 }
 
-export default Services
+export default Services;
